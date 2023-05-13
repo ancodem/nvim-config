63161d600833432bdfd446ad5c4598613fb671f9
Damir Akhmadullin <damir@damir.local>
2023-05-13 22:38:08 +0300

    Whichkey options

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -9,12 +9,15 @@ wk.register({
         w = {"Find Text"}
     },
     e = {"File Manager"},
-    o = {"Git status"},
     x = {"Close Buffer"},
     w = {"Save"},
     t = {name = "Terminal", f = {"Float terminal"}, h = {"Horizontal terminal"}},
     h = {"No highlight"},
-    g = {name = "Git", b = "Branches", c = "Commits", s = "Status"},
+    g = {
+        name = "Git",
+        b = "Branches",
+        c = "Commits",
+        s = "Status"},
     c = {name = "Comment", l = "Comment Line"},
     l = {
         name = "LSP",
@@ -24,5 +27,16 @@ wk.register({
         r = "Rename",
         a = "Action",
         s = "Symbol"
+    },
+    p = {
+        name = "Plugin",
+        p = "Open Lazy",
+        m = "Open Mason",
+        u = "Mason update",
+    },
+    u = {
+        name = "UI",
+        a = "Toggle autosave",
     }
+    
 }, {prefix = "<leader>"})
